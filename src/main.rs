20288b72a928a58c35901c0e818c27fb7339c515
Joshy Orndorff <admin@joshyorndorff.com>
2021-06-29 13:41:10 -0400

    print the results

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use sc_executor::read_embedded_version;
 use sc_executor_common::runtime_blob::RuntimeBlob;
 
 fn main() {
-    println!("Hello, world!");
-
     // The code needs to be in "raw" format, not hex.
     // TODO find a way to convert if hex is provided.
     let code = &include_bytes!("../moonbase.wasm")[..];
@@ -15,7 +13,7 @@ fn main() {
         .expect("Should be able to decode any custom wasm sections encountered")
         .expect("Custom wasm section containing the runtime version should be present");
 
-    assert_eq!(version.spec_name, "moonbase".into());
-
-    assert_eq!(version.spec_version, 53);
+    println!("spec name: {}", version.spec_name);
+    println!("spec version: {}", version.spec_version);
+    println!("impl version: {}", version.impl_version);
 }
